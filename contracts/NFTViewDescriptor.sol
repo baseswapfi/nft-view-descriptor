420620724d92cf6bb306c890065d206bf88b22de
0xshoegazer <0xshoegazer@gmail.com>
2024-01-06 17:06:12 -0500

    refactor to util function

--- contracts/NFTViewDescriptor.sol
+++ contracts/NFTViewDescriptor.sol
@@ -31,15 +31,10 @@ contract NFTViewDescriptor is AccessControl, INFTViewDescriptor {
          * TODO: Add any requireed checks.
          * So this contract has the option to be used by other systems/contracts without repetitive concerns/checks
          */
-
         INFTPool pool = INFTPool(nftPoolAddress);
         IMasterChef chef = pool.master();
 
-        // Check for active pool
-        (, uint256 allocPoints, uint256 allocPointsWETH, , , , , ) = chef.getPoolInfo(nftPoolAddress);
-
-        if (!pool.exists(tokenId)) revert InvalidTokenId();
-        if (allocPoints == 0 && allocPointsWETH == 0) revert InvalidPool();
+        _validatePosition(pool, tokenId);
 
         uint8 lockTier = 10;
         uint8 boostTier = 10;
@@ -51,4 +46,29 @@ contract NFTViewDescriptor is AccessControl, INFTViewDescriptor {
         nftDescription.boostTier = boostTier;
         nftDescription.owner = pool.ownerOf(tokenId);
     }
+
+    function _validatePosition(INFTPool pool, uint256 tokenId) internal view {
+        if (!pool.exists(tokenId)) revert InvalidTokenId();
+
+        // Check for active pool
+        (, uint256 allocPoints, uint256 allocPointsWETH, , , , , ) = pool.master().getPoolInfo(address(pool));
+
+        if (allocPoints == 0 && allocPointsWETH == 0) revert InvalidPool();
+    }
+
+    // function getPositionLockTier(INFTPool pool) public view returns (uint8) {
+    //     uint8 lockTier = 10;
+
+    //     if (pool.isUnlocked()) lockTier = 0;
+
+    //     return lockTier;
+    // }
+
+    // function getPositionBoostTier(INFTPool pool, IMasterChef chef) public view returns (uint8) {
+    //     uint8 boostTier = 10;
+
+    //     if (chef.yieldBooster() == address(0)) boostTier = 0;
+
+    //     return 0;
+    // }
 }
